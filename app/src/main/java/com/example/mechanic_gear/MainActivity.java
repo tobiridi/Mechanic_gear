5b61319d623981154458d5f9658acbf809ba19a1
tobiridi <tony.6141@hotmail.com>
2022-06-10 17:46:18 +0200

    start main activity
    - search bar + button
    - list of all gears

--- app/src/main/java/com/example/mechanic_gear/MainActivity.java
+++ app/src/main/java/com/example/mechanic_gear/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.mechanic_gear;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout ll_all_gears = findViewById(R.id.ll_all_gears);
+
+        for (int i = 1; i <= 20; i++) {
+            TextView textView = new TextView(MainActivity.this);
+            View v_divider = new View(MainActivity.this);
+            v_divider.setMinimumWidth(ll_all_gears.getWidth() - 10);
+            v_divider.setMinimumHeight(2);
+            v_divider.setBackgroundColor(getResources().getColor(R.color.black));
+
+            textView.setText("je suis le texte " + i);
+            textView.setTextSize(30);
+            textView.setPadding(10,10,10,20);
+            //textView.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+
+            ll_all_gears.addView(textView);
+            ll_all_gears.addView(v_divider);
+
+        }
 
     }
 }
