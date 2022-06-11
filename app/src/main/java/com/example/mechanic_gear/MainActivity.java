29de41fec13b7a2dfd2eea122cf299a50aaf317d
tobiridi <tony.6141@hotmail.com>
2022-06-11 09:05:32 +0200

    modify main activity
    - modify colors
    - change content Scroll View

--- app/src/main/java/com/example/mechanic_gear/MainActivity.java
+++ app/src/main/java/com/example/mechanic_gear/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.mechanic_gear;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.GridLayout;
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,23 +17,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LinearLayout ll_all_gears = findViewById(R.id.ll_all_gears);
+        View v_divider = findViewById(R.id.v_divider);
 
         for (int i = 1; i <= 20; i++) {
-            TextView textView = new TextView(MainActivity.this);
-            View v_divider = new View(MainActivity.this);
-            v_divider.setMinimumWidth(ll_all_gears.getWidth() - 10);
-            v_divider.setMinimumHeight(2);
-            v_divider.setBackgroundColor(getResources().getColor(R.color.black));
 
-            textView.setText("je suis le texte " + i);
-            textView.setTextSize(30);
-            textView.setPadding(10,10,10,20);
-            //textView.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+            Drawable imageView = ResourcesCompat.getDrawable(getResources(), R.drawable.add_a_photo, null);
+            //resize drawable
+            imageView.setBounds(0,0,120,120);
+            //imageView.setPadding(10,10,10,10);
+            //imageView.setImageResource(R.drawable.add_new_item);
+            //imageView.setLayoutParams(ll_all_gears.getLayoutParams());
 
-            ll_all_gears.addView(textView);
-            ll_all_gears.addView(v_divider);
+            Button btn = new Button(MainActivity.this);
+            btn.setText("sonde de température");
+            btn.setCompoundDrawables(imageView, null, null, null);
+            btn.setBackground(ResourcesCompat.getDrawable(getResources(), R.drawable.button_custom_background, null));
 
+
+            View v_multiple_divider = new View(MainActivity.this);
+            //v_multiple_divider.setBackground(v_divider.getBackground());
+            v_multiple_divider.setLayoutParams(v_divider.getLayoutParams());
+            v_multiple_divider.setVisibility(View.INVISIBLE);
+
+            //ll_all_gears.addView(imageView);
+            //ll_all_gears.addView(v_multiple_divider);
+
+            ll_all_gears.addView(btn);
+            ll_all_gears.addView(v_multiple_divider);
         }
-
     }
 }
